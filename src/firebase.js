f675325a70ea391339b9ef2102ffbc0420b4b09d
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2023-11-08 13:46:17 +0800

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFvvW_sx9sKOaIaLmLc3CH7ADPRIZr-2g",
+  authDomain: "esport-tournament-tools.firebaseapp.com",
+  projectId: "esport-tournament-tools",
+  storageBucket: "esport-tournament-tools.appspot.com",
+  messagingSenderId: "636848675435",
+  appId: "1:636848675435:web:e7dd627a514bf717946f2e",
+  measurementId: "G-JDEF2S7FXL"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
