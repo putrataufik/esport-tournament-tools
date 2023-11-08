f6f7228230979f8c23c89eaf80c829fa222a2d0a
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2023-11-08 20:26:25 +0800

    add new components
    Navbar, StartTourButton and TournamentCard

---
+++ src/components/StartTourButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StartTourButton = () => {
+  return (
+    <div className='w-[340px] h-[64px] bg-[#E36B6B] rounded-lg flex justify-center items-center'>
+      <p className='text-xl text-white font-bold cursor-pointer select-none'>Start Your Tournament</p>
+    </div>
+  )
+}
+
+export default StartTourButton
