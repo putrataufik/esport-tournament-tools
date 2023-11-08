bba03618705a91c6409acb7e7826072e14cf570d
putrataufik <daengkadaeng@gmail.com>
2023-11-08 17:02:29 +0800

    undo

--- src/services/authentication/login.js
+++ src/services/authentication/login.js
@@ -3,6 +3,5 @@ import { signInWithEmailAndPassword } from "firebase/auth"
 
 export const login = async (email, password) => {
   const response = await signInWithEmailAndPassword(auth, email, password).then((res) => res.user);
-
   return response
 }
